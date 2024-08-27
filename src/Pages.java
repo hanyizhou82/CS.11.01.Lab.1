82295d3f97821873cb6ca7044ae4b8ae0d59a0a3
hanyizhou <hanyiz2027@student.cis.edu.hk>
2024-08-27 13:38:00 +0800

    alterations

--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
 public class Pages {
 
     public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        // Prompt the user to enter their age
+        System.out.print("Enter your age: ");
+        int age = reader.nextInt();
+        reader.close();
+        // Calculate the number of pages to read before giving up on a book
+        int pagesToRead = 100 - age;
 
+        // Display the result
+        System.out.println(age + "-year olds should read at least " + pagesToRead + " pages before giving up on a book.");
     }
 }
